727e6c6b906d40fbadf140aa6b544e501a008457
Fauzan Abdul Hakim <48903232+zhansky@users.noreply.github.com>
2020-02-06 21:10:21 +0700

    file

---
+++ app/src/main/java/com/fauzan/kafeserver/common/Common.java
@@ -0,0 +1,8 @@
+package com.fauzan.kafeserver.common;
+
+import com.fauzan.kafeserver.model.ServerUserModel;
+
+public class Common {
+    public static final String SERVER_REF = "Server";
+    public static ServerUserModel currentServerUser;
+}
